cbfce170688a46ab65ea0d7490f99a352721ba6f
s0njablade <sonjita.chacon@gmail.com>
2018-11-29 16:50:36 -0700

    ammended environment const

--- database-connections.js
+++ database-connections.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_DEV || 'development'
+const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')
 const configEnv = config[environment]
 const knex = require ('knex')
